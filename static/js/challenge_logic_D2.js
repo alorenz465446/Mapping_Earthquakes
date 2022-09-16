46733615497a38d0572daa854c60d42892add153
alorenz465446 <107652317+alorenz465446@users.noreply.github.com>
2022-09-16 14:54:02 -0400

    ...

--- static/js/challenge_logic_D2.js
+++ static/js/challenge_logic_D2.js
@@ -188,6 +188,56 @@ d3.json(earthquakeData).then(function (data) {
         }
     }).addTo(earthquakes);
 
+    // calling major earthquaole data
+
+    d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson").then(function (majorData) {
+
+        // 4. Use the same style as the earthquake data.
+        function styleInfo(feature) {
+            return {
+                opacity: 1,
+                fillOpacity: 1,
+                fillColor: getColor(feature.properties.mag),
+                color: "#000000",
+                radius: getRadius(feature.properties.mag),
+                stroke: true,
+                weight: 0.5
+            };
+        }
+
+        // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+        function getColor(magnitude) {
+            if (magnitude > 6) {
+                return "#ea2c2c";
+            }
+            if (magnitude > 5) {
+                return "#ea822c";
+            }
+            if (magnitude < 5) {
+                return "#ee9c00";
+            }
+
+        }
+
+        // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+        function getRadius(magnitude) {
+            if (magnitude === 0) {
+                return 1;
+            }
+            return magnitude * 4;
+        }
+
+        // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+        // sets the style of the circle, and displays the magnitude and location of the earthquake
+        //  after the marker has been created and styled.
+        L.geoJson(majorData, {
+
+        });
+        // 8. Add the major earthquakes layer to the map.
+
+        // 9. Close the braces and parentheses for the major earthquake data.
+    });
+
     // Create a legend control object.
     let legend = L.control({
         position: "bottomright"
@@ -236,8 +286,7 @@ d3.json(earthquakeData).then(function (data) {
 
     });
 
-// link to major earthquake data 
-// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson
+
 
 
     // no data beyond this point
